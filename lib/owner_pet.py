7e6a2fa8901881799c944cfc63f980f6a49282e3
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-27 13:25:02 -0400

    lab complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,25 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name: str, pet_type: str, owner=""):
+        self.name = name
+        self.owner = owner
+        self.pet_type = pet_type
+        if pet_type not in self.PET_TYPES:
+            raise Exception("Pet must be in approved list")
+        self.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name: str):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        pet.owner = self
+    
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda x: x.name)
